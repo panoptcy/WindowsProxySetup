efaaa41768bdbffa972ab8adc15319f94bf2cdde
Brandon Poole <POOLEBC@cohosd00001177.ds.sc.edu>
2013-08-17 16:34:30 -0400

    2nd Commit

--- proxy.ps1
+++ proxy.ps1
@@ -4,11 +4,11 @@ Set-ItemProperty -Path $path -Name "MigrateProxy" -Value "1"
 If(-not(Test-Path -Path $path))
   {
    New-Item -Path $path -Force
-   New-ItemProperty -Path $path -Name "ProxyServer" -Value "129.252.46.48:3128"
+   New-ItemProperty -Path $path -Name "ProxyServer" -Value "IPAddress:Port"
   }
 Else
   {
-   Set-ItemProperty -Path $path -Name "ProxyServer" -Value "129.252.46.48:3128"
+   Set-ItemProperty -Path $path -Name "ProxyServer" -Value "IPAddress:Port"
   }
 
 
